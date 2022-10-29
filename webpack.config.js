9ab0f0e37acc7181a688b3a73e4c84dca0c013e2
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2022-10-29 00:03:42 -0300

    chore: Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge');
+const singleSpaDefaults = require('webpack-config-single-spa-react-ts');
+const path = require('path');
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: 'portfolio',
+    projectName: 'main',
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    resolve: {
+      alias: {
+        '@app': path.resolve(__dirname, 'src/'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@core': path.resolve(__dirname, 'src/core'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+      },
+    },
+  });
+};
