287ebad835929a2b273e21a4af0386447113948e
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2022-10-31 20:19:10 -0300

    chore: config sonar

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -6,7 +6,7 @@ function Users() {
         className="p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800"
         role="alert"
       >
-        <span className="font-medium">Info alert!</span>
+        <span className="font-medium text-clifford">Info alert!</span>
         {' '}
         Change a few things up and try submitting again.
       </div>
