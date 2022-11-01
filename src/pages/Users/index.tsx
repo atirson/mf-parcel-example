5598eba3d673dba718728500598f32cde77caa02
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2022-11-01 18:04:19 -0300

    chore: config sonar

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,7 +1,7 @@
 function Users() {
   return (
     <div>
-      <h1>Users</h1>
+      <h1>Users Sonar</h1>
       <div
         className="p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800"
         role="alert"
