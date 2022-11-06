98a0d5c2ae45a318078c4cef2c6b53576073b8a6
Atirson Fabiano <fabiano.oliveira2213@gmail.com>
2022-11-06 16:11:10 -0300

    config: added colors variables and resolve path absolute config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,29 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: '@storybook/react',
+
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@app': path.resolve(__dirname, '../src/'),
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@core': path.resolve(__dirname, '../src/core'),
+      '@assets': path.resolve(__dirname, '../src/assets'),
+      '@pages': path.resolve(__dirname, '../src/pages'),
+    };
+
+    return config;
+  },
+
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+};
